9a4542fc2f54a454a13e8691e3180018cb50d17d
PavelSadykov <2439747@gmail.com>
2023-07-17 19:20:56 +0500

    Добавьте файлы проекта.

---
+++ ClassJournal/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ClassJournal
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.dateTimePickerBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.listBoxStudents = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(34, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(112, 45);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(204, 20);
+            this.textBoxName.TabIndex = 1;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dateTimePickerBirthDate
+            // 
+            this.dateTimePickerBirthDate.Location = new System.Drawing.Point(116, 104);
+            this.dateTimePickerBirthDate.Name = "dateTimePickerBirthDate";
+            this.dateTimePickerBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerBirthDate.TabIndex = 2;
+            this.dateTimePickerBirthDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(417, 45);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(87, 79);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click_1);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(569, 44);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(85, 80);
+            this.buttonRemove.TabIndex = 4;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click_1);
+            // 
+            // listBoxStudents
+            // 
+            this.listBoxStudents.FormattingEnabled = true;
+            this.listBoxStudents.Location = new System.Drawing.Point(116, 191);
+            this.listBoxStudents.Name = "listBoxStudents";
+            this.listBoxStudents.Size = new System.Drawing.Size(200, 95);
+            this.listBoxStudents.TabIndex = 5;
+            this.listBoxStudents.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxStudents);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dateTimePickerBirthDate);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBirthDate;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.ListBox listBoxStudents;
+    }
+}
+
---
+++ ClassJournal/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ClassJournal
+{
+    public partial class MainForm : Form
+    {
+        private List<Student> students;
+        public string xmlFilePath = "students.xml";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadStudents();
+        }
+
+        public void LoadStudents()
+        {
+            if (File.Exists(xmlFilePath))
+            {
+               XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+                using (FileStream fileStream = new FileStream(xmlFilePath, FileMode.Open))
+                {
+                    students = (List<Student>)serializer.Deserialize(fileStream);
+                }
+                UpdateListBox();
+            }
+            else
+            {
+                students = new List<Student>();
+            }
+        }
+
+        private void UpdateListBox()
+        {
+            listBoxStudents.DataSource = null;
+            listBoxStudents.DataSource = students.OrderBy(s => s.Name).ToList();
+            listBoxStudents.DisplayMember = "Name";
+        }
+
+       
+
+        
+
+        private void SaveStudentsToXml()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+            using (FileStream fileStream = new FileStream(xmlFilePath, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, students);
+            }
+        }
+
+        private void buttonAdd_Click_1(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            DateTime birthDate = dateTimePickerBirthDate.Value;
+
+            Student student = new Student
+            {
+                Name = name,
+                BirthDate = birthDate
+            };
+
+            students.Add(student);
+            UpdateStudentNumbers();
+            UpdateListBox();
+            SaveStudentsToXml();
+        }
+
+        private void buttonRemove_Click_1(object sender, EventArgs e)
+        {
+            if (listBoxStudents.SelectedItem != null)
+            {
+                Student selectedStudent = (Student)listBoxStudents.SelectedItem;
+                students.Remove(selectedStudent);
+                UpdateStudentNumbers();
+                UpdateListBox();
+                SaveStudentsToXml();
+            }
+        }
+        private void UpdateStudentNumbers()
+        {
+            students = students.OrderBy(s => s.Name).ToList();
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                students[i].Number = i + 1;
+            }
+        }
+     
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+
+    }
+    [Serializable]
+    public class Student
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
